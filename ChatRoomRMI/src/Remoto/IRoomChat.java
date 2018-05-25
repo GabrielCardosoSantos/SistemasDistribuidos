45689583e1c58dfe7a001c99b97e68369262f1ff
GabrielCardosoSantos <gcsantos@inf.ufsm.br>
2018-05-25 15:11:59 -0300

    Att
    Att

---
+++ ChatRoomRMI/src/Remoto/IRoomChat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Remoto;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author gabri
+ */
+public interface IRoomChat extends Remote{
+    public void sendMsg(String usrName, String msg) throws RemoteException;
+    public void joinRoom(String usrName) throws RemoteException;
+    public void leaveRoom(String usrName) throws RemoteException;
+    public void closeRoom() throws RemoteException;
+}
